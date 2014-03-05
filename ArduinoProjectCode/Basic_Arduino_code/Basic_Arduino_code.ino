cc9e142501347f3b5ccdb12ecd023f4ab3d1ddfe
benwoodcock <ben.bwoodcock.woodcock@gmail.com>
2014-03-05 14:28:40 -0800

    Added more comments

--- ArduinoProjectCode/Basic_Arduino_code/Basic_Arduino_code.ino
+++ ArduinoProjectCode/Basic_Arduino_code/Basic_Arduino_code.ino
@@ -1,21 +1,21 @@
 //Written by Ben Woodcock
 #include <Servo.h> // references inbuilt arduino library 
-const int tempsensorPin = A1;
+const int tempsensorPin = A1;// this line states that the temperature sensor data will be coming in on the analog port 1
 const float baselineTemp = 32.0; //This sets the base line temperature at 32 degrees
-const int lightSensorPin = A0;
-const int pingPin = 13;
-const int motorPin1 = 9;
-const int motorPin2 = 8;
+const int lightSensorPin = A0; // this line states that the light sensor data will be coming in on the analog port 0 
+const int pingPin = 13; //these lines of code set what pins are being used and by naming them it makes it easier to change 
+const int motorPin1 = 9;//these lines of code set what pins are being used and by naming them it makes it easier to change
+const int motorPin2 = 8;//these lines of code set what pins are being used and by naming them it makes it easier to change
 Servo myservo;
-int Fan = 3; 
-int lightSensorValue = 0;
-int InfraredLed = 12;
-int inPin = 11;
-int safeZone = 30;
-int LEDpins1= 1;
-int LEDpins2= 2;
-int LEDlights1 = 4;
-int LEDlights2 = 5;
+int Fan = 3; //these lines of code set what pins are being used and by naming them it makes it easier to change
+int lightSensorValue = 0; // This sets the light sensor value to 0 when starting 
+int InfraredLed = 12;//these lines of code set what pins are being used and by naming them it makes it easier to change
+int inPin = 11;//these lines of code set what pins are being used and by naming them it makes it easier to change
+int safeZone = 30; // this states that anywhere from 0 to 30 is the safezone
+int LEDpins1= 1;//these lines of code set what pins are being used and by naming them it makes it easier to change
+int LEDpins2= 2;//these lines of code set what pins are being used and by naming them it makes it easier to change
+int LEDlights1 = 4;//these lines of code set what pins are being used and by naming them it makes it easier to change
+int LEDlights2 = 5;//these lines of code set what pins are being used and by naming them it makes it easier to change
 
 void setup(){
  Serial.begin(9600); 
@@ -70,32 +70,32 @@ void loop(){
 
   digitalWrite(pingPin, LOW);
   delayMicroseconds(2);              
-  digitalWrite(pingPin, HIGH);
+  digitalWrite(pingPin, HIGH);// this part of code sends out a ping to measure the distance from the ultrasonic sensor
   delayMicroseconds(5);
   digitalWrite(pingPin, LOW);
   
   
-  duration = pulseIn(inPin, HIGH);
+  duration = pulseIn(inPin, HIGH);// this line waits for the ping to come back to the sensor and also times how long it took the ping to come back
   
   cm = microsecondsToCentimeters(duration);
   Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
+  Serial.print("cm"); // this section converts how long it took the ping to come back and then  multiplys that value to the speed it was sent out by and thats the calculation for distance 
+  Serial.println(); // then it takes the value and puts cm on it to make it a distance value
   
-  if (cm > safeZone)
+  if (cm > safeZone) // if the distance is greater than the safezone (30cm) then the two motors will be high making the robot move forward
   {
   digitalWrite(motorPin1, HIGH);
   digitalWrite(motorPin2, HIGH); 
   }
-  else
+  else // however if the distance is shorter than the safezone then the servo motor will turn to 0 degrees
   {
     myservo.write(0);
   }
-  delay(100);
+  delay(1000);
 
   digitalWrite(pingPin, LOW);
-  delayMicroseconds(2);            // sends out signal from ultrasonic sensor - if more than safezone value, turn motor 1 on - if less than safezone value, the servo head turns 170 degrees (turns the other way)
-  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(2);            
+  digitalWrite(pingPin, HIGH);// sends out signal from ultrasonic sensor - if more than safezone value, turn motor 1 on - if less than safezone value, the servo head turns 170 degrees (turns the other way)
   delayMicroseconds(5);
   digitalWrite(pingPin, LOW);
   
@@ -104,8 +104,8 @@ void loop(){
   
   cm = microsecondsToCentimeters(duration);
   Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
+  Serial.print("cm");// this section converts how long it took the ping to come back and then  multiplys that value to the speed it was sent out by and thats the calculation for distance 
+  Serial.println();// then it takes the value and puts cm on it to make it a distance value
   
   if (cm > safeZone)
   {
@@ -130,8 +130,8 @@ void loop(){
   
   cm = microsecondsToCentimeters(duration);
   Serial.print(cm);
-  Serial.print("cm");                                         // calculations
-  Serial.println();
+  Serial.print("cm");   // this section converts how long it took the ping to come back and then  multiplys that value to the speed it was sent out by and thats the calculation for distance 
+  Serial.println();// then it takes the value and puts cm on it to make it a distance value
   
   if (cm > safeZone)
   {
